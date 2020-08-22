4c07274313f5e6ae5a8065011af3eb350ff109f4
QIFU <Qifu1995@outlook.com>
2020-08-22 19:13:14 +1000

    create CURD method for product router

--- api/router/products.js
+++ api/router/products.js
@@ -2,7 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-	res.send("This is all products.");
+	res.status(200).json({ method: `${req.method}`, msg: "Get all products." });
+});
+router.get("/:productID", function (req, res) {
+	let productID = req.params.productID;
+	res.status(200).json({ method: `${req.method}`, msg: `Get the product No ${productID}.` });
+});
+router.post("/", function (req, res) {
+	res.status(200).json({ method: `${req.method}`, msg: "Create a new product." });
+});
+router.put("/:productID", function (req, res) {
+	let productID = req.params.productID;
+	res.status(200).json({ method: `${req.method}`, msg: `Update the product No ${productID}.` });
+});
+router.delete("/:productID", function (req, res) {
+	let productID = req.params.productID;
+	res.status(200).json({ method: `${req.method}`, msg: `Delete the product No ${productID}.` });
 });
 
 module.exports = router;
