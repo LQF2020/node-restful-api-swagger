4b814521882b11f35f5caee8f5be6fd92b12b59f
QIFU <Qifu1995@outlook.com>
2020-08-22 20:29:41 +1000

    add error handler

--- app.js
+++ app.js
@@ -14,4 +14,21 @@ var orderRouter = require("./api/router/orders");
 
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
+
+// No route found error handler
+app.use((req, res, next) => {
+	const error = new Error("Not Found.");
+	error.status = 404;
+	next(error);
+});
+
+// Handle all errors, including DB connection error, api error...
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		errCode: err.status,
+		error: err.message,
+	});
+});
+
 module.exports = app;
