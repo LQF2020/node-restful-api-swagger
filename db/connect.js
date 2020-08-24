cbb5cc5c830d1282bb3e927788f2e6f7e2aee23c
QIFU <Qifu1995@outlook.com>
2020-08-25 00:16:52 +1000

    create connect.js file for db connection

---
+++ db/connect.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { DB_HOST } = process.env;
+const { DB_USER } = process.env;
+const { DB_PASS } = process.env;
+const { DB_NAME } = process.env;
+
+mongoose.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const db = mongoose.connection;
+
+const dbConn = () => {
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+        console.log('DB is connected!');
+    });
+};
+
+module.exports = dbConn;
